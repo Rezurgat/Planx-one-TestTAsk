183663e4fb3e11703bb6e16f637aac525d93c510
Rezurgat <Killa.S.V.ru@bk.ru>
2022-11-08 21:17:57 +0300

    Add routes for work with swagger and redoc; refactored config/urls

--- expense_assistant/config/urls.py
+++ expense_assistant/config/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from config.routes import urlpatterns as yasg_urls
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+
+    path("api/v1/", include("user.urls")),
+    path("api/v1/", include("category.urls")),
+    path("api/v1/", include("transaction.urls")),
+] + yasg_urls
+
