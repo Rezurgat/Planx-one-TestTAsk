b4ea2d01cef82fd99046bbfcc6c183ac3966d6c1
Rezurgat <Killa.S.V.ru@bk.ru>
2022-11-10 20:06:18 +0300

    Refactored task_2.sql

--- sql_task/task_2.sql
+++ sql_task/task_2.sql
@@ -1,6 +1,6 @@
-SELECT ROUND(CAST(width as numeric), -1)  as width, 
-ROUND(CAST(depth as numeric), -1) as depth,
-ROUND(CAST(height as numeric), -1) as height,
+SELECT CEILING(CAST(width as numeric)) + ((5 - CEILING(CAST(width as numeric)) % 5)) % 5  as width,
+CEILING(CAST(depth as numeric)) + ((5 - CEILING(CAST(depth as numeric)) % 5)) % 5 as depth,
+CEILING(CAST(height as numeric)) + ((5 - CEILING(CAST(height as numeric)) % 5)) % 5 as height,
 COUNT(*) as count
 FROM notebooks_notebook
 GROUP BY 1, 2, 3
